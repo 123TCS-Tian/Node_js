6ff3f084f53256648b1923b95d52bb278b42d1fc
Aya <yt@123tcs.io>
2021-05-05 10:23:35 +0200

    node.js study

---
+++ my-app/app.js
@@ -0,0 +1,17 @@
+// const log = require('./logger')
+// log('message')
+
+// const path = require('path');
+
+// var pathObject = path.parse(__filename)
+
+// console.log(pathObject);
+
+console.log('start');
+function alp() {
+  console.log('alp')
+}
+setTimeout(() => {
+  console.log('settime');
+}, 1000)
+console.log('end');
---
+++ my-app/logger.js
@@ -0,0 +1,9 @@
+
+console.log(__filename);
+var url = 'http://xiaomixio.com';
+
+function log(message){
+  console.log(message)
+}
+
+module.exports=log
